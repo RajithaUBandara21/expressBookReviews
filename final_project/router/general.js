7c85544a12117890750c7cd23f6f2378ab56fcef
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-05-28 17:20:23 +0530

    3-getbooksbyauthor Task 3 compleat

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let books = require("./booksdb.js");
+const books = require('./booksdb.js');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -20,16 +20,25 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  let filtered_users = users.filter((user) => user.isbn === isbn);
-  res.send(filtered_users);
+  for (let key in books) {
+    if (books[key].isbn === isbn) {
+      return res.status(200).json(books[key]);
+    }
+  }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let bookList = [];
+  for (let key in books) {
+    if (books[key].author === author) {
+      bookList.push(books[key]);
+    }
+  }return res.status(200).json(bookList);
 });
-
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
