99ec24c336b905b86481ef6978577eea4fe95a1c
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-05-28 18:09:55 +0530

    Task 01 recorrect

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,12 +21,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   for (let key in books) {
-    if (books[key].isbn === isbn) {
+    if (key === isbn) {
       return res.status(200).json(books[key]);
-    }
-  }
-
- });
+  }}});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -54,7 +51,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
 });
 
 module.exports.general = public_users;
